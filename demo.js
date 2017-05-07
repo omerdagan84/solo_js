1998f73917d5a14724f31a71d7204eb20dd2ee6c
Omer Dagan <omerdagan84@gmail.com>
2017-05-07 23:44:01 +0300

    added loops

--- demo.js
+++ demo.js
@@ -7,12 +7,21 @@ alert("My alert box - Demo");
 
 // after alert is done print the variable to screen
 var x = 100;
-document.write(x);
+document.write(x + "<br />");
 
-var MyString = "this 'my' string - and it contines quotes";
-document.write(MyString);
+var MyString = "this 'my' string - and it containes quotes";
+document.write(MyString + "<br />");
 
 //string concatination
 var mystring1 = "I am learning ";
 var mystring2 = "JavaScript with SoloLearn.";
-document.write(mystring1 + mystring2);
+document.write(mystring1 + mystring2 + "<br />");
+for (i=1; i<=5; i++) {
+	   document.write(i + "<br />");
+}
+var j=20;
+do {  
+	  document.write(j + "<br />");
+	    j++;  
+}
+while (j<=25); 
