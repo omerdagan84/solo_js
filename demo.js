ab2986ea0064f0c781af0433d153c41277a7481a
Omer Dagan <omerx.dagan@intel.com>
2017-05-07 15:25:32 +0300

    first
    Change-Id: Ie1e72121ae84de3ca18e4ed1eaa48ef02bdefbb6

---
+++ demo.js
@@ -0,0 +1,18 @@
+// javascript single line comment 
+alert("My alert box - Demo");
+/* javascript multiline comment
+ * somthing of this
+ * sor
+ */
+
+// after alert is done print the variable to screen
+var x = 100;
+document.write(x);
+
+var MyString = "this 'my' string - and it contines quotes";
+document.write(MyString);
+
+//string concatination
+var mystring1 = "I am learning ";
+var mystring2 = "JavaScript with SoloLearn.";
+document.write(mystring1 + mystring2);
