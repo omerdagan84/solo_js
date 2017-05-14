03e0a919b8b16f9ae9c820f3624081d2d8ff0876
Omer Dagan <omerdagan84@gmail.com>
2017-05-14 22:51:24 +0300

    finished object section

--- object.js
+++ object.js
@@ -15,15 +15,26 @@ document.write(age_a + " " + age_b + "<br />");
 function person(name, age) {
 	this.name = name;
 	this.age = age;
+	this.yob = bornYear;
+	this.changeName = function (name) {
+		this.name = name;
+	}
 }
 
+function bornYear() {
+	return 2016 - this.age;
+}
 var person_a = new person("John", 21);
 var person_b = new person("Amy", 22);
 
 document.write("IS THIS WORKING");
 
 document.write(person_a.name);   
+person_a.changeName("Dave");
+document.write(person_a.name);   
 document.write(person_b.name);   
+document.write(person_b.yob());   
+
 var course = {
 	name: "JS", 
 	lessons: 41
